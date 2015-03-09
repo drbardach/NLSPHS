77c09498f63e0077d507c969eb198fbfed31d69e
David R. Bardach <drbardach@gmail.com>
2015-03-09 10:42:01 -0400

    Begin
    Started project and Git repository

---
+++ Creating Data.R
@@ -0,0 +1,8 @@
+## Read in formatted dataset
+NLSPHS2014_formated <- read.csv("/Volumes/CCPHSSR/xDATA/NLSPHS 2014/Analysis/NLSPHS2014_formated.csv")
+
+
+## To Do:
+# -assign group membership
+# -merge data
+# -clean data
