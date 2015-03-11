4e12dbe01e2917c9cd2710681d256930abbe5948
David R. Bardach <drbardach@gmail.com>
2015-03-11 11:46:17 -0400

    Sample Visualizations
    Added sample shiny app and sample python graph

--- Creating Data.R
+++ Creating Data.R
@@ -1,9 +1,8 @@
 ## Read in formatted dataset
+## (formatted dataset is created by Lava's SAS Code)
 NLSPHS2014_formated <- read.csv("/Volumes/CCPHSSR/xDATA/NLSPHS 2014/Analysis/NLSPHS2014_formated.csv")
 
 
 ## To Do:
-# -assign group membership
 # -merge data
 # -clean data
-# -sync with Lava
